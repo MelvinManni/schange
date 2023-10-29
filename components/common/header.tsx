25882c499627f76a7ea94c99abe11c86125219b2
Melvin <melvinnnamani@gmail.com>
2023-10-29 13:44:10 +0100

    added favicon and logo component

---
+++ components/common/header.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { LogoIcon } from "../SVG";
+
+export default function Header() {
+  return (
+    <div className="mx-auto max-w-7xl px-2 py-3 sm:px-6 lg:px-8 flex justify-center">
+        <LogoIcon width={120} height={"100%"} />
+    </div>
+  );
+}
