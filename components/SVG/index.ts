7b2dd97a1a2a3dd2b5c314560f94d4ad015cddcd
Melvin <melvinnnamani@gmail.com>
2023-10-30 11:49:27 +0100

    rename componenet

--- components/SVG/index.ts
+++ components/SVG/index.ts
@@ -1,3 +1,3 @@
-export { default as LogoIcon } from "./logo";
+export { default as LogoIcon } from "./app-logo";
 export { default as LongArrowRightIcon } from "./arrow-right";
 export { default as SpinnerIcon } from "./spinner";
